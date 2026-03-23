02125afa1ccb999cdbcea783cded0eee0a6b6b27
souleid <souleid@gmail.com>
2026-03-23 22:34:10 +0900

    project init commit

---
+++ Source/IndianPoker/IndianPoker.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "IndianPoker.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IndianPoker, "IndianPoker" );
+ 
